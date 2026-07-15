f89bf40dd5e50e1c3d702ee687d41f7701863675
Mahsa-Vaziri <mahsavaziri62@gmail.com>
2026-07-14 20:23:36 -0400

    Initial project structure

---
+++ app/main.py
@@ -0,0 +1,8 @@
+print("===================================")
+print(" Welcome to AI BA Workspace 🚀")
+print("===================================")
+
+name = input("What is your name? ")
+
+print(f"Welcome {name}!")
+print("Let's build AI together.")   
